c0090898db2f1a27c0dec386e4db81b067c40f90
camilopinoh <camilo.pino12@inacapmail.cl>
2022-12-11 21:41:46 -0300

    Login con admin y users

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
 const userSchema = require("../models/user");
+const adminSchema = require("../models/admin");
 
 const router = express.Router();
 
@@ -16,25 +17,51 @@ router.post("/login", async (req, res) => {
   const { error } = schemaLogin.validate(req.body);
   if (error) return res.status(400).json({ error: error.details[0].message });
 
-  // Validaciond e existencia
+  // Validacion de existencia
+
   const user = await userSchema.findOne({ email: req.body.email });
-  if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
-
-  // Validacion de password en la base de datos
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword)
-    return res.status(400).json({ error: "Constraseña invalida" });
-
-  res.json({
-    error: null,
-    data: "Succesfull login",
-    status: 200,
-    rut: user.rut,
-    role: user.role,
-    name: user.name,
-    lastName: user.lastName,
-    email: user.email,
-  });
+  if (user) {
+    // Validacion de password en la base de datos
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).json({ error: "Constraseña invalida" });
+    res.json({
+      error: null,
+      data: "Succesfull login",
+      status: 200,
+      rut: user.rut,
+      role: user.role,
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+    });
+  }
+  if (!user) {
+    const admin = await adminSchema.findOne({ email: req.body.email });
+    if (admin) {
+      // Validacion de password en la base de datos
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        admin.password
+      );
+      if (!validPassword)
+        return res.status(400).json({ error: "Constraseña invalida" });
+      res.json({
+        error: null,
+        data: "Succesfull login",
+        status: 200,
+        rut: admin.rut,
+        role: admin.role,
+        name: admin.name,
+        lastName: admin.lastName,
+        email: admin.email,
+      });
+    }
+    if (!admin) return res.status(400).json({ error: "Usuario no encontrado" });
+  }
 });
 
 module.exports = router;
